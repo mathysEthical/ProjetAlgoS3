ff33041080448df3f949ed017c1d043e206d4dcc
mathysEthical <110499907+mathysEthical@users.noreply.github.com>
2024-11-07 19:20:27 +0100

    Dice proba ladder

--- source/Program.cs
+++ source/Program.cs
@@ -8,17 +8,36 @@ public class Alphabet{
 
 public class Dice{
     char letter;
-    Plateau board;
-    char[] alphabet={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    
+    char[] alphabet;
+    Random rnd;
+    int[] lettersProbas;
     public char Letter{
         get{
             return this.letter;
         }
     }
 
-    public Dice(Plateau board){
-        this.board=board;
+    public int ladderValueToIndex(int value){
+        int index=0;
+        int actualLadderValue=0;
+        while(actualLadderValue+this.lettersProbas[index]<value){
+            actualLadderValue+=this.lettersProbas[index];
+            index++;
+        }
+        return index;
+    }
+
+    int sumOfIntArray(int[] array){
+        int total=0;
+        for(int i=0;i<array.Length;i++){
+            total+=array[i];
+        }
+        return total;
+    }
+    public Dice(Random rnd,char[] alphabet,int[] lettersProbas){
+        this.rnd=rnd;
+        this.alphabet=alphabet;
+        this.lettersProbas=lettersProbas;
         this.Roll();
     }
 
@@ -27,7 +46,7 @@ public class Dice{
     }
 
     public char Roll(){
-        this.letter=alphabet[this.board.rnd.Next(0, alphabet.Length)];
+        this.letter=alphabet[ladderValueToIndex(this.rnd.Next(0, sumOfIntArray(this.lettersProbas)))];
         return this.Letter;
     }
 }
@@ -35,17 +54,21 @@ public class Dice{
 public class Plateau{
     Dice[] dices;
     int size;
+    int [] lettersProbas;
+    char[] alphabet;
     public Random rnd = new Random();
     public Dice[] Dices{
         get{
             return this.dices;
         }
     }
-    public Plateau(int size){
+    public Plateau(int size,char[] letters,Dictionary<char,int> lettersScores,int[] lettersProbas){
         this.size=size;
+        this.lettersProbas=lettersProbas;
+        this.alphabet=letters;
         this.dices=new Dice[size*size];
         for(int i=0;i<size*size;i++){
-            this.dices[i]=new Dice(this);
+            this.dices[i]=new Dice(rnd,this.alphabet, this.lettersProbas);
         }
     }
 
@@ -59,8 +82,6 @@ public class Plateau{
         }
         return toReturn;
     }
-
-    //todo: ToString
 }
 
 public class Program
@@ -77,16 +98,36 @@ public class Program
         return toReturn;
     }
 
-    public static Dictionary<char,int> LoadLetterScore(){
-        Dictionary <char,int> toReturn=new Dictionary<char, int>();
-        string[] scoreArray=LoadFile("scores.txt").Split('\n');
-        for(int i=0;i<scoreArray.Length;i++){
-            string[] lineArray=scoreArray[i].Split(' ');
-            string letter=lineArray[0];
-            int ratio=int.Parse(lineArray[1]);
-            toReturn.Add(letter[0],ratio);
+    public static int[] LoadLettersProbas(string[] lettersContentArray){
+        int[] probas=new int[lettersContentArray.Length];
+        for(int i=0;i<lettersContentArray.Length;i++){
+            string[] lineArray=lettersContentArray[i].Split(';');
+            int proba=int.Parse(lineArray[2]);
+            probas[i]=proba;
         }
-        return toReturn;
+        return probas;
+    }
+
+    public static char[] LoadLettersAlphabet(string[] lettersContentArray){
+        char[] lettersAlphabet=new char[lettersContentArray.Length];
+        for(int i=0;i<lettersContentArray.Length;i++){
+            string[] lineArray=lettersContentArray[i].Split(';');
+            char character=char.Parse(lineArray[0]);
+            lettersAlphabet[i]=character;
+        }
+        return lettersAlphabet;
+    }
+
+    public static Dictionary<char,int> LoadLettersScore(string[] lettersContentArray){
+        Dictionary <char,int> letterScores=new Dictionary<char, int>();
+        
+        for(int i=0;i<lettersContentArray.Length;i++){
+            string[] lineArray=lettersContentArray[i].Split(';');
+            string letter=lineArray[0];
+            int score=int.Parse(lineArray[1]);
+            letterScores.Add(letter[0],score);
+        }
+        return letterScores;
     }
 
     public static int scoreFromWorld(string word,Dictionary <char,int> letterScores){
@@ -99,8 +140,11 @@ public class Program
     }
     public static void Main(string[] args)
     {
-        Dictionary <char,int> letterScores=LoadLetterScore();
-        Plateau board=new Plateau(4);
+        string[] lettersContentArray = LoadFile("lettres.txt").Split('\n');
+        char[] lettersAlphabet= LoadLettersAlphabet(lettersContentArray);
+        Dictionary <char,int> letterScores=LoadLettersScore(lettersContentArray);
+        int[] lettersProbas=LoadLettersProbas(lettersContentArray);
+        Plateau board=new Plateau(4,lettersAlphabet,letterScores,lettersProbas);
         Console.WriteLine(board);
     }
 
